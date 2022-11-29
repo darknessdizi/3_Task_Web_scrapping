4d8b1a20469dde51f1931a92443e8abdd5ec643d
Remezov Dmitriy <darknessdizi@gmail.com>
2022-11-30 05:05:08 +0700

    наведен порядок в функциях, код сделан более читабельным

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def get_headers():
 
     return Headers(browser='chrome', os='win').generate()
 
+
 def get_parametres(num):
 
     '''Параметры для get запросов'''
@@ -25,33 +26,22 @@ def get_parametres(num):
         'hhtmFrom': 'vacancy_search_list'
     }
 
+
 def get_requests(url, params=None, class_=None, **kwargs):
 
-    '''Осуществяет get запросы, поиск тегов по html объекту и
+    '''Осуществяет get запрос, создает объект BeautifulSoup и осуществляет 
     
-    поиск заданных параметров в описании'''
+    поиск тега по html объекту с проверкой на ненулевое значение'''
 
     while True:
         html = requests.get(url, headers=get_headers(), params=params)
         soup = BeautifulSoup(html.text, features="lxml")
         div = soup.find('div', class_=class_, attrs=kwargs)
         if div:
-            if params != None:
-                serp_items = div.find_all('div', class_="serp-item", 
-                                        attrs=[attrs1, attrs2, attrs3])
-                return div, soup, serp_items
-            else:
-                description = soup.find('div', attrs={"class": [
-                                                    "vacancy-branded-user-content", 
-                                                    "vacancy-description"
-                                                    ]}).text
-                pattern1 = re.search(r'[Dd]jango', description)
-                pattern2 = re.search(r'[Ff]lask', description)
-                if pattern1 or pattern2: 
-                    append_list(div)
-                return
+            return div, soup
 
-def append_list(div):
+
+def append_list(div, my_dict, item):
 
     '''Добавляет данные в словарь и в общий список словарей'''
 
@@ -64,19 +54,45 @@ def append_list(div):
     my_json.append(my_dict)
     save_json()
 
+
 def save_json():
 
     '''Сохраняет список словарей в json файл'''
 
     with open('web_parser.json', 'w', encoding='UTF-8') as f:
-        json.dump(my_json, f, ensure_ascii=False, indent=2)   
+        json.dump(my_json, f, ensure_ascii=False, indent=2)
 
 
-if __name__ == '__main__':
-    
-    a = time.perf_counter()
+def create_bar_and_find_tegs(serp_items):
+
+    '''Создает прогресс-бар и ищет совпадения в описании объявлений'''
+
+    with alive_bar(len(serp_items), force_tty=True, dual_line=True) as bar:
+        for item in serp_items:
+            my_dict = {}
+            my_dict['link'] = item.find('a').get('href')
+            bar.text = f'Download {item.find("a").text}, please wait ...'
+            div, soup = get_requests(my_dict['link'], class_="vacancy-title")
+            description = soup.find(attrs={"class": [
+                                                    "vacancy-branded-user-content", 
+                                                    "vacancy-description"
+                                                    ]}).text
+            pattern1 = re.search(r'[Dd]jango', description)
+            pattern2 = re.search(r'[Ff]lask', description)
+            if pattern1 or pattern2: 
+                append_list(div, my_dict, item)
+            bar()
+
+
+def main():
+
+    '''Проверяет наличие страниц и запускает основные функции'''
+
+    end_page = True
+    count_page = 0
     while end_page:
-        div, soup, serp_items = get_requests(url, get_parametres(count_page), id="a11y-main-content")
+        div, soup = get_requests(url, get_parametres(count_page), id="a11y-main-content")
+        serp_items = div.find_all(class_="serp-item", attrs=[attrs1, attrs2, attrs3])
         end_page = soup.find(attrs={"data-qa": "pager-next"})
         if end_page:
             print('Страница: ', count_page + 1)
@@ -85,17 +101,16 @@ if __name__ == '__main__':
             print('Страница: ', count_page + 1)
             end_page = False
             
-        with alive_bar(len(serp_items), force_tty=True, dual_line=True) as bar:
-            for item in serp_items:
-                my_dict = {}
-                my_dict['link'] = item.find('a').get('href')
-                bar.text = f'Download {item.find("a").text}, please wait ...'
-                get_requests(my_dict['link'], class_="vacancy-title")
-                bar()
+        create_bar_and_find_tegs(serp_items)
                 
         print('-' * 80)
         count_page += 1
-                
+
+
+if __name__ == '__main__':
+    
+    a = time.perf_counter()
+    main()       
     b = time.perf_counter()
     print('Время работы:', b - a)
             
--- variables.py
+++ variables.py
@@ -4,8 +4,6 @@ attrs1 = {"data-qa": "vacancy-serp__vacancy vacancy-serp__vacancy_premium"}
 attrs2 = {"data-qa": "vacancy-serp__vacancy vacancy-serp__vacancy_standard_plus"}
 attrs3 = {"data-qa": "vacancy-serp__vacancy vacancy-serp__vacancy_standard"}
 
-count_page = 0
-end_page = True
 my_json = []
 
 if __name__ == '__main__':
